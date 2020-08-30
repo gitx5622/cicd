b8f9381416c14687bd3b47e43995779494c9036f
gitx5622 <ggitau5622@gmail.com>
2020-08-30 11:30:20 +0300

    db connection

--- config/db.php
+++ config/db.php
@@ -6,6 +6,7 @@ return [
     'username' => 'bazengadaddy',
     'password' => 'Password123#@!',
     'charset' => 'utf8',
+    'emulatePrepare'=>true,
 
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
