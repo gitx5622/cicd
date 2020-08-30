abd8d986d137b3f097708dfca025b836b89256f2
gitx5622 <ggitau5622@gmail.com>
2020-08-31 00:24:59 +0300

    db connection

--- config/db.php
+++ config/db.php
@@ -2,12 +2,11 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=cicd_db;dbname=cicd',
+    'dsn' => 'mysql:host=db;dbname=cicd',
     'username' => 'bazengadaddy',
     'password' => 'Password123#@!',
     'charset' => 'utf8',
-    'tablePrefix'=>'t_',
-
+    'attributes' => [PDO::ATTR_CASE => PDO::CASE_LOWER],
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
     //'schemaCacheDuration' => 60,
