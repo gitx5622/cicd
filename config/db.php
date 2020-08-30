9759ac711787bc0729334a972f8d5808166b1d26
gitx5622 <ggitau5622@gmail.com>
2020-08-30 11:55:51 +0300

    db connection

--- config/db.php
+++ config/db.php
@@ -5,8 +5,7 @@ return [
     'dsn' => 'mysql:host=172.26.0.2;dbname=cicd;port:3307',
     'username' => 'bazengadaddy',
     'password' => 'Password123#@!',
-    'charset' => 'utf8',
-    'emulatePrepare'=>true,
+    'charset' => 'utf8mb4',
 
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
